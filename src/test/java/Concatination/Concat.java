d6b3f1df4e352acf6ebc1f3c944aed870a6f8664
Neha Nadaf <nehnadaf@publicisgroupe.net>
2022-11-30 00:14:15 +0530

    Assignment added

---
+++ src/test/java/Concatination/Concat.java
@@ -0,0 +1,16 @@
+package Concatination;
+
+import org.testng.annotations.Test;
+
+public class Concat {
+	
+    @Test
+	public void assign(){
+		String firstname= "Neha";
+		String lastname="Nadaf";
+		System.out.println("Hello "+ firstname  +" "+ lastname);
+		
+
+	}
+
+}
